dff47a1106f07adae8e46669254fdcd3865cb56d
hernanofx <hernandorra@gmail.com>
2026-01-13 13:51:06 -0300

    fix4

--- frontend/docker/start.sh
+++ frontend/docker/start.sh
@@ -17,8 +17,16 @@ fi
 echo "Backend host configured as: $BACKEND_HOST"
 
 # Use envsubst to substitute environment variables in nginx config
-envsubst '$BACKEND_HOST' < /etc/nginx/conf.d/default.conf > /etc/nginx/conf.d/default.conf.tmp
-mv /etc/nginx/conf.d/default.conf.tmp /etc/nginx/conf.d/default.conf
+# If BACKEND_HOST is still the default (backend:8000) we avoid creating a proxy to an unresolved host
+if [ "$BACKEND_HOST" = "backend:8000" ]; then
+  echo "BACKEND_HOST is default (backend:8000); configuring /api/v1 to return 502 until real backend is provided"
+  # Replace the proxy_pass line with a simple 502 response so nginx won't fail to start
+  sed 's/proxy_pass http:\/\/\$BACKEND_HOST;/return 502;/' /etc/nginx/conf.d/default.conf > /etc/nginx/conf.d/default.conf.tmp
+  mv /etc/nginx/conf.d/default.conf.tmp /etc/nginx/conf.d/default.conf
+else
+  envsubst '$BACKEND_HOST' < /etc/nginx/conf.d/default.conf > /etc/nginx/conf.d/default.conf.tmp
+  mv /etc/nginx/conf.d/default.conf.tmp /etc/nginx/conf.d/default.conf
+fi
 
 echo "Starting nginx..."
 exec nginx -g 'daemon off;'
